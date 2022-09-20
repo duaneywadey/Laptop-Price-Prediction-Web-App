4b6e251ba094322ffe0300c6118b4911e72f5dea
duaneywadey <idrdequito.cvt@eac.edu.ph>
2022-09-21 07:06:22 +0800

    changed UI

--- app.py
+++ app.py
@@ -31,21 +31,6 @@ def resultPage():
 	return render_template('result.html', data=predInt)
 
 
-# @app.route('/predict', methods=['POST'])
-# def resultPage():
-# 	data1 = request.form['a']	
-# 	data2 = request.form['b']
-# 	data3 = request.form['c']
-# 	data4 = request.form['d']
-# 	data5 = request.form['e']
-# 	data6 = request.form['f']
-# 	data7 = request.form['g']
-# 	arr = np.array([[data1, data2, data3, data4, data5, data6, data7]], dtype=float)
-# 	pred = model.predict(arr)
-# 	predInt = int(pred)
-# 	return render_template('after.html', data=predInt)
-
-# print(type(model))
 
 if __name__ == "__main__":
     app.run(debug=True)
